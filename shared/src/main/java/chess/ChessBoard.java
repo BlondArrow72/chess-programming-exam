f0cb274b2577bdf678339ccd481fa5fdbabce731
BlondArrow72 <fletcherbsmith@gmail.com>
2025-01-28 09:34:19 -0700

    Implemented ChessBoard and passed 2 of 3 tests
    The remaining test requires that ChessPieceMoveCalculator subcalculators
    not thrown an assertion error, which right now they do because they're
    not implemented yet.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -8,10 +11,10 @@ package chess;
  */
 public class ChessBoard {
 
-    private[][] ChessPiece chessBoard;
+    private ChessPiece[][] board;
 
     public ChessBoard() {
-        chessBoard = new ChessPiece()[8][8];
+        board = new ChessPiece[8][8];
     }
 
     /**
@@ -21,7 +24,10 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        int row = position.getRow();
+        int col = position.getColumn();
+
+        board[row-1][col-1] = piece;
     }
 
     /**
@@ -32,7 +38,10 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        int row = position.getRow();
+        int col = position.getColumn();
+
+        return board[row-1][col-1];
     }
 
     /**
@@ -40,6 +49,53 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        // make board completely null
+        board = new ChessPiece[8][8];
+
+        // create a helper array
+        ChessPiece.PieceType[] helperArray = {
+                ChessPiece.PieceType.ROOK,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.QUEEN,
+                ChessPiece.PieceType.KING,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.ROOK
+        };
+
+        // initialize board
+        for (int i = 0; i < 8; i++) {
+            // WHITE starting row
+            board[0][i] = new ChessPiece(ChessGame.TeamColor.WHITE, helperArray[i]);
+
+            // Pawns
+            board[1][i] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+            board[6][i] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+
+            // BLACK ending row
+            board[7][i] = new ChessPiece(ChessGame.TeamColor.BLACK, helperArray[i]);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessBoard{" +
+                "board=" + Arrays.deepToString(board) +
+                '}';
     }
 }
