2376ac072ebee83d6f077e66373b71cd68e71cf0
BlondArrow72 <fletcherbsmith@gmail.com>
2025-01-28 10:18:09 -0700

    Implemented KingMovesCalculator and passed all tests

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -1,9 +1,33 @@
 package chess;
 
 import java.util.Collection;
+import java.util.ArrayList;
 
 public class KingMovesCalculator implements ChessPieceMovesCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> possibleMoves = new ArrayList<ChessMove>();
+
+        int myRow = myPosition.getRow();
+        int myCol = myPosition.getColumn();
+
+        int[][] directions = {
+                { 1, -1}, { 1, 0}, { 1, 1},
+                { 0, -1},          { 0, 1},
+                {-1, -1}, {-1, 0}, {-1, 1}
+        };
+
+        for (int i = 0; i < 8; i++) {
+            int[] newDirection = directions[i];
+            int newRow = myRow + newDirection[0];
+            int newCol = myCol + newDirection[1];
+            ChessPosition newPosition = new ChessPosition(newRow, newCol);
+            ChessMove newMove = new ChessMove(myPosition, newPosition);
+
+            if (isInBounds(newPosition)) {
+                evaluateMove(possibleMoves, board, newMove);
+            }
+        }
+
+        return possibleMoves;
     }
 }
