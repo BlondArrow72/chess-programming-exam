57a823c382b76e55d0c865141a81bada072993c9
BlondArrow72 <fletcherbsmith@gmail.com>
2025-01-28 10:27:58 -0700

    Implemented BishopMovesCalculator and passed all tests

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -5,6 +5,87 @@ import java.util.ArrayList;
 
 public class BishopMovesCalculator implements ChessPieceMovesCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> possibleMoves = new ArrayList<>();
+
+        int myRow = myPosition.getRow();
+        int myCol = myPosition.getColumn();
+
+        // Bishops can move up and to the right
+        for(int i = 1; i <= 7; i++) {
+            ChessPosition targetPosition = new ChessPosition(myRow+i, myCol+i);
+            ChessMove newMove = new ChessMove(myPosition, targetPosition);
+
+            if (isInBounds(targetPosition)) {
+                if (canCapture(board, myPosition, targetPosition)) {
+                    possibleMoves.add(newMove);
+                    break;
+                }
+                else if (isBlocked(board, myPosition, targetPosition)) {
+                    break;
+                }
+                else {
+                    possibleMoves.add(newMove);
+                }
+            }
+        }
+
+        // Bishops can move down and to the right
+        for(int i = 1; i <= 7; i++) {
+            ChessPosition targetPosition = new ChessPosition(myRow-i, myCol+i);
+            ChessMove newMove = new ChessMove(myPosition, targetPosition);
+
+            if (isInBounds(targetPosition)) {
+                if (canCapture(board, myPosition, targetPosition)) {
+                    possibleMoves.add(newMove);
+                    break;
+                }
+                else if (isBlocked(board, myPosition, targetPosition)) {
+                    break;
+                }
+                else {
+                    possibleMoves.add(newMove);
+                }
+            }
+        }
+
+        // Bishops can move down and to the left
+        for(int i = 1; i <= 7; i++) {
+            ChessPosition targetPosition = new ChessPosition(myRow-i, myCol-i);
+            ChessMove newMove = new ChessMove(myPosition, targetPosition);
+
+            if (isInBounds(targetPosition)) {
+                if (canCapture(board, myPosition, targetPosition)) {
+                    possibleMoves.add(newMove);
+                    break;
+                }
+                else if (isBlocked(board, myPosition, targetPosition)) {
+                    break;
+                }
+                else {
+                    possibleMoves.add(newMove);
+                }
+            }
+        }
+
+        // Bishops can move up and to the left
+        for(int i = 1; i <= 7; i++) {
+            ChessPosition targetPosition = new ChessPosition(myRow+i, myCol-i);
+            ChessMove newMove = new ChessMove(myPosition, targetPosition);
+
+            if (isInBounds(targetPosition)) {
+                if (canCapture(board, myPosition, targetPosition)) {
+                    possibleMoves.add(newMove);
+                    break;
+                }
+                else if (isBlocked(board, myPosition, targetPosition)) {
+                    break;
+                }
+                else {
+                    possibleMoves.add(newMove);
+                }
+            }
+        }
+
+        return possibleMoves;
     }
 }
